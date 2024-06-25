041533313db053da5449f006cdaba2208dfc8e8c
Daniel-Manjarrez <dam2274@columbia.edu>
2024-06-25 18:08:15 +0000

    app.py changes

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
-import os
+import requests
+import json
+import sqlalchemy as db
+import pandas as pd
+from datetime import datetime
 
-KEY = os.environ.get('OPENAI_KEY')
+api_key = '8YQ53Ao5sqOGEq826OfsK3PqOEQBWY36Iv0KJsTx'
+base_url = 'https://api.watchmode.com/v1/title/'
 
-print(KEY)
+def fetch_data(api_key, search_query):
+    url = f"{base_url}{search_query}/details/?apiKey={api_key}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error fetching data: {response.status_code}")
+        return None
 
+if __name__ == '__main__':
+    search_query = '3173903' # This should return "Breaking Bad"
 
-def Program():
-
-    print("Welcome to Python Pokedex!")
-
-    while True:
-        choice = input("What can I help you with today?: ")
-
-
-Program()
+    # Fetch data from Watchmode API
+    data = fetch_data(api_key, search_query)
+    print(data)
